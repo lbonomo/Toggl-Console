b663486938e1a867cb981bfbda220fe19db7f983
Lucas M. Bonomo <lucas@vgnet.com.ar>
2018-09-18 18:26:04 -0300

    Jugando con Angular Material

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -2,11 +2,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 // Components
 import { SettingsComponent } from './components/settings/settings.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const APP_ROUTES:Routes = [
     { path: 'settings', component: SettingsComponent },
+    { path: 'home', component: DashboardComponent },
+
     // Default route
-    { path: '**', pathMatch: 'full', redirectTo: 'settings' }
+    { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ]
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatCardModule
+ } from '@angular/material';
+
 import { NgModule } from '@angular/core';
 // Necesario para poder utiliar NgForm
 import { FormsModule } from '@angular/forms'
 
+// Routes
+import { APP_ROUTING } from './app-routes';
 
 // Components
 import { SettingsComponent } from './components/settings/settings.component';
-
-// Routes
-import { APP_ROUTING } from './app-routes';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 
 @NgModule({
     declarations: [
-        SettingsComponent
+        SettingsComponent,
+        DashboardComponent
     ],
     imports: [
         BrowserModule,
         FormsModule,
+
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatCheckboxModule,
+        MatIconModule,
+        MatToolbarModule,
+        MatMenuModule,
+        MatCardModule
+
+
+
         APP_ROUTING,
     ],
     providers: [],
-    bootstrap: [SettingsComponent]
+    bootstrap: [DashboardComponent]
 })
 export class AppModule { }
