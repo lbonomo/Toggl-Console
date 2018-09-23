af34e6c3062bef1ca001b1ea7b134cf3618efcdf
Lucas M. Bonomo <lucas@vgnet.com.ar>
2018-09-23 19:36:53 -0300

    Mejorando la estetica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+// Material
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import {
